86b7e6e53d07e41945955d6a7db7f6e19357b804
Taryn Lewis <tarynslewis@gmail.com>
2018-01-23 11:13:35 -0800

    Add function to randomize card indexes

--- designs.js
+++ designs.js
@@ -15,3 +15,24 @@ function makeGrid() {
 	}
 
 }
+
+Array.prototype.shuffle = function() {
+
+	var input = this;
+
+	for (var i = input.length-1; i>=0; i--) {
+
+		var randomIndex = Math.floor(Math.random()*(i+1));
+		var itemAtIndex = input[randomIndex];
+
+		input[randomIndex] = input[i];
+		input[i] = itemAtIndex;
+	}
+
+	return input;
+}
+
+var shuffleNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+shuffleNums.shuffle();
+
+alert(shuffleNums);
